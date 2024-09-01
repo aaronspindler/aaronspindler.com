3813416bc705c6f9a24a5245a92f9d591bb054a0
Aaron Spindler <aaron@xnovax.net>
2024-09-01 19:34:22 -0400

    Added json field for geo data

--- pages/models.py
+++ pages/models.py
@@ -6,6 +6,7 @@ class PageVisit(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     ip_address = models.GenericIPAddressField()
     page_name = models.CharField(max_length=255)
+    geo_data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.page_name} visited from {self.ip_address} at {self.created_at}"
