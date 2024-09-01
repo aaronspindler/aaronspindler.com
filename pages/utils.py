59fc522c863d4b0a11f605d49bf644d9c86a569a
Aaron Spindler <aaron@xnovax.net>
2024-09-01 19:25:01 -0400

    Added books

--- pages/utils.py
+++ pages/utils.py
@@ -21,3 +21,77 @@ def get_blog_from_template_name(template_name):
         "blog_content": blog_content,
         "template_name": template_name,
     }
+
+def get_books():
+    books = []
+    books.append({
+        "name": "Dark Mirror",
+        "cover_image": "images/dark-mirror.jpg",
+    })
+    books.append({
+        "name": "The Little Book of Market Wizards",
+        "cover_image": "images/the-little-book-of-market-wizards.jpg",
+    })
+    books.append({
+        "name": "Long Range Shooting Handbook",
+        "cover_image": "images/long-range-handbook.jpg",
+    })
+    books.append({
+        "name": "The Inevitable",
+        "cover_image": "images/the_inevitable.jpg",
+    })
+    books.append({
+        "name": "Data for the People",
+        "cover_image": "images/data-for-the-people.jpg",
+    })
+    books.append({
+        "name": "Django 3 By Example",
+        "cover_image": "images/django-3-by-example.png",
+    })
+    books.append({
+        "name": "Permanent Record",
+        "cover_image": "images/permanent-record.jpg",
+        "favourite_quote": "Credit cards are like little personal trackers, tracking your every move"
+    })
+    books.append({
+        "name": "Principles",
+        "cover_image": "images/principles.jpg",
+    })
+    books.append({
+        "name": "Django For Professionals",
+        "cover_image": "images/djangoforprofessionals.jpg",
+        "favourite_quote": "Searching and sorting can be very complex, but it doesn't have to be!"
+    })
+    books.append({
+        "name": "Cracking the Coding Interview",
+        "cover_image": "images/crackingthecodinginterview.jpg",
+        "favourite_quote": "This is going to be a hard one"
+    })
+    books.append({
+        "name": "Assume the Worst",
+        "cover_image": "images/assumetheworst.jpg",
+        "favourite_quote": "You've probably never been told this, but you are going to fail. Over and over again!"
+    })
+    books.append({
+        "name": "Six Not So Easy Pieces",
+        "cover_image": "images/6notsoeasypieces.jpg",
+    })
+    books.append({
+        "name": "Six Easy Pieces",
+        "cover_image": "images/6easypieces.jpg",
+    })
+    books.append({
+        "name": "Blockchain",
+        "cover_image": "images/blockchain-the-next-everything.jpg",
+    })
+    books.append({
+        "name": "Make",
+        "cover_image": "images/make.jpg",
+    })
+    books.append({
+        "name": "The Healthy Programmer",
+        "cover_image": "images/healthyprogrammer.jpeg",
+    })
+    books.sort(key=lambda x: x['name'], reverse=False)
+
+    return books
--- pages/views.py
+++ pages/views.py
@@ -2,33 +2,13 @@ from django.http import HttpResponse
 from django.template import TemplateDoesNotExist
 from django.shortcuts import render
 
-from pages.utils import get_blog_from_template_name
+from pages.utils import get_blog_from_template_name, get_books
 
 import os
 from django.conf import settings
 import logging
 
 logger = logging.getLogger(__name__)
-
-
-def home(request):
-    logger.info("Home page requested")
-    blog_posts = []
-    blog_templates_path = os.path.join(settings.BASE_DIR, 'templates', 'blog')
-    for template_name in os.listdir(blog_templates_path):
-        if template_name.endswith('.html'):
-            template_name = template_name.split('.')[0]
-            blog_posts.append(get_blog_from_template_name(template_name))
-    blog_posts.sort(key=lambda x: x['created_timestamp'], reverse=True)
-    return render(request, "pages/home.html", {"blog_posts": blog_posts})
-
-
-def render_blog_template(request, template_name):
-    try:
-        blog_data = get_blog_from_template_name(template_name)
-        return render(request, "_blog_base.html", blog_data)
-    except TemplateDoesNotExist:
-        return render(request, "404.html")
 
 def robotstxt(request):
     lines = [
@@ -38,3 +18,50 @@ def robotstxt(request):
     ]
 
     return HttpResponse("\n".join(lines), content_type="text/plain")
+
+def home(request):
+    logger.info("Home page requested")
+    # Blog
+    blog_posts = []
+    blog_templates_path = os.path.join(settings.BASE_DIR, 'templates', 'blog')
+    for template_name in os.listdir(blog_templates_path):
+        if template_name.endswith('.html'):
+            template_name = template_name.split('.')[0]
+            blog_posts.append(get_blog_from_template_name(template_name))
+    blog_posts.sort(key=lambda x: x['created_timestamp'], reverse=True)
+    
+    # Projects
+    projects = []
+    projects.append({
+        "name": "Team Bio",
+        "description": "Team Bio is a platform to foster professional connections between coworkers within a company. This is done with profiles, trivia, coffee chats, and more.",
+        "link": "https://team.bio",
+        "tech": ["Python", "Django", "PostgreSQL", "HTML", "JavaScript"]
+    })
+    projects.append({
+        "name": "Poseidon",
+        "description": "Poseidon is a tool to help explore financial data, generate insights, and make trading decisions.",
+        "link": "https://github.com/aaronspindler/Poseidon",
+        "tech": ["Python", "Django", "PostgreSQL", "C#", "Prophet", "Various ML/AI models"]
+    })
+    
+    # Books
+    books = get_books()
+    
+    return render(
+        request,
+        "pages/home.html",
+        {
+            "blog_posts": blog_posts,
+            "projects": projects,
+            "books": books
+        }
+    )
+
+
+def render_blog_template(request, template_name):
+    try:
+        blog_data = get_blog_from_template_name(template_name)
+        return render(request, "_blog_base.html", blog_data)
+    except TemplateDoesNotExist:
+        return render(request, "404.html")
