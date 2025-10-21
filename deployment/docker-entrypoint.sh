836e35abfae8d1f5c35a8331004d30d381c1b9f5
Aaron Spindler <aaron@xnovax.net>
2025-10-20 22:15:00 -0400

    Update docker-entrypoint.sh

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -17,7 +17,7 @@ python manage.py build_css || {
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic || {
+python manage.py collectstatic --no-input || {
     echo "Warning: Static file collection failed, but continuing..."
 }
 
