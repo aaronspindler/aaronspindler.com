4d2ca467dda8bb661e5e8103089f47166257abf1
Aaron Spindler <aaron@xnovax.net>
2025-12-14 20:51:10 -0500

    Enhance URL routing and update JavaScript file reference
    - Added admin URL path for staff access and included authentication routes for accounts using allauth.
    - Updated utility URLs to include the utils app.
    - Changed the JavaScript file reference for German translation tooltips from a minified version to the unminified version for easier debugging.

--- omas/urls.py
+++ omas/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 
 app_name = "omas"
 
 urlpatterns = [
+    # Omas Coffee home page
     path("", views.home, name="home"),
+    # Admin (required for staff access)
+    path("admin/", admin.site.urls),
+    # Authentication (required for allauth)
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
+    # Utility URLs (for search, lighthouse, etc.)
+    path("", include("utils.urls")),
 ]
